c15db8a548f88cbcc51c9583c04d776e204c34ec
Harkirat Singh <harkirat1892@gmail.com>
2017-03-09 16:19:39 +0530

    Final

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# priority_queue
+Dependencies:
+------------
+
+dateutil:
+    sudo pip3 install python-dateutil
+
+Python3
+
+
+Assuming the input data is in the format as in sample.csv
---
+++ queueProcess.py
@@ -0,0 +1,114 @@
+from dateutil import parser
+import time
+import sys
+
+class Task:
+    def __init__(self, name = "", time = "1789/11/11 0:00", priority = -1):
+        self.name = name
+        self.time = parser.parse(time)
+        self.priority = priority
+
+class priorityQueue:
+
+    def __init__(self, Arr):
+        self.Arr = [0]
+
+        self.Arr.extend(Arr)
+        
+        self.length = len(Arr)
+        
+        self.build_max_heap()
+
+    def maximum(self):
+        #return the root element of heap O(1)
+        return self.Arr[1]
+
+    def pop(self):
+        #To extract the task with maximum priority O(logN)
+        if self.length == 0:
+            print("No tasks to pop!")
+        
+        _max = self.Arr[1]
+
+        self.Arr[1] = self.Arr[self.length]
+
+        self.length = self.length - 1
+
+        self.max_heapify(1)
+
+        return _max
+
+    def max_heapify(self, i):
+        #O(Log N)
+        left = 2 * i
+        right = 2 * i + 1
+
+        if((left <= self.length) and ((self.Arr[left].priority > self.Arr[i].priority) or (self.Arr[left].time < self.Arr[i].time))):
+            largest = left
+        else:
+            largest = i
+
+        if((right <= self.length) and ((self.Arr[right].priority > self.Arr[largest].priority) or (self.Arr[right].time < self.Arr[largest].time))):
+            largest = right
+
+        if(largest != i):
+            self.swap(i, largest)
+            
+            self.max_heapify(largest)
+
+    def build_max_heap(self):
+        #O(N)
+        for i in range(int(self.length/2), 0, -1):
+            self.max_heapify(i)
+        
+    def swap(self, i, j):
+        
+        tmp = self.Arr[i]
+
+        self.Arr[i] = self.Arr[j]
+        
+        self.Arr[j] = tmp
+
+    
+def parseCsv(name):
+    result = []
+    
+    with open(name) as f:
+        lis=[line.strip().split(",") for line in f]
+        for x in enumerate(lis):
+            if(len(x[1]) > 1):
+                if(len(x[1]) ==2):
+                    x[1].append(-1)
+                else:
+                    x[1][2] = (int(x[1][2].strip()))
+
+                _task = Task(x[1][0].strip()[1:-1], x[1][1].strip()[1:-1], x[1][2])
+                result.append(_task)
+    
+    return result
+
+
+
+
+a = parseCsv(sys.argv[1])
+
+current_time = parser.parse(sys.argv[2])
+
+a = [x for x in a if x.time >= current_time]
+
+tasks = priorityQueue(a)
+
+for i in range(0, tasks.length):
+    if(current_time == None):
+        current_time = tasks.Arr[1].time
+    
+    tmp = tasks.Arr[1].time
+    
+    if(tmp > current_time):
+        diff = (tmp - current_time).total_seconds()
+        time.sleep(diff)
+        current_time = tmp
+        print("-- After another " + str(diff/60) + " minute --")
+
+    y = tasks.pop()
+    print("Current time [ " +  str(y.time) + "  ] , Event " + str(y.name) + " Processed")
